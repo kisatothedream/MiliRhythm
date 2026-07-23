0c34fa2958e7e0191d77a79b5bca5164b10343d5
Yeonghui Choe <yeonghui.choe@kongstudios.net>
2026-07-23 10:00:29 +0900

    차트 데이터 추가

--- Assets/Scripts/MilliRhythm/Rhythm/RhythmChart.cs
+++ Assets/Scripts/MilliRhythm/Rhythm/RhythmChart.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 namespace MilliRhythm.Rhythm
 {
+	public enum Difficulty
+	{
+		Easy,
+		Normal,
+		Hard,
+		Expert,
+	}
+
 	[Serializable]
 	[CreateAssetMenu(fileName = "RhythmChart", menuName = "Rhythm Game/Rhythm Chart")]
 	public class RhythmChart : ScriptableObject
 	{
+		[field: SerializeField] public int MusicId;
+		[field: SerializeField] public Difficulty Difficulty;
+
 		[SerializeField] private AudioClip audioClip;
 		[SerializeField, HideInInspector] private double bpm;
 		[SerializeField, HideInInspector] private double offsetSeconds;
