d24b86fb8c6ac882e1061f0f663b51f8216903d8
Yeonghui Choe <yeonghui.choe@kongstudios.net>
2026-08-12 11:24:42 +0900

    요요 스케일러 유틸리티 추가

---
+++ Assets/Scripts/MilliRhythm/UI/Util/UIYoyoScaler.cs
@@ -0,0 +1,33 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace MilliRhythm.UI.Util
+{
+	public class UIYoyoScaler : MonoBehaviour
+	{
+		[SerializeField, Range(0.01f, 10f)] private float targetScale;
+		[SerializeField, Range(0.01f, 10f)] private float duration;
+		private RectTransform rect;
+		private Tween yoyoTween;
+
+		private void Awake()
+		{
+			rect = GetComponent<RectTransform>();
+			yoyoTween = rect.DOScale(targetScale, duration)
+				.Pause()
+				.SetEase(Ease.InOutSine)
+				.SetLoops(-1, LoopType.Yoyo)
+				.SetAutoKill(false);
+		}
+
+		private void OnEnable()
+		{
+			yoyoTween.Restart();
+		}
+
+		private void OnDisable()
+		{
+			yoyoTween.Rewind();
+		}
+	}
+}
