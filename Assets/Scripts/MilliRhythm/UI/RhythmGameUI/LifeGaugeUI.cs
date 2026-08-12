70ce1d55f773784a08c34da359b8f8d7860b8979
Yeonghui Choe <yeonghui.choe@kongstudios.net>
2026-08-12 14:21:28 +0900

    라이프 게이지에 아이콘 추가
    # Conflicts:
    #	Assets/Scenes/RhythmGameScene.unity

--- Assets/Scripts/MilliRhythm/UI/RhythmGameUI/LifeGaugeUI.cs
+++ Assets/Scripts/MilliRhythm/UI/RhythmGameUI/LifeGaugeUI.cs
@@ -6,10 +6,17 @@ namespace MilliRhythm.UI.RhythmGameUI
 	public class LifeGaugeUI : MonoBehaviour
 	{
 		[SerializeField] private Image gaugeFill;
+		[SerializeField] private Image faceIconImage;
+		[SerializeField] private RectTransform faceIconRect;
 
 		public void UpdateGauge(int cur, int max)
 		{
 			gaugeFill.fillAmount = (float)cur / (float)max;
+			var anchor = faceIconRect.anchorMin;
+			anchor.y = (float)cur / max;
+
+			faceIconRect.anchorMin = anchor;
+			faceIconRect.anchorMax = anchor;
 		}
 	}
 }
