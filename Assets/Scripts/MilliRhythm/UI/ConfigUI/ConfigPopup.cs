2a2c6c8056d6bd6104ff46fde7ceaf8a3f6165c6
Kisa98M1 <kisatothedream@gmail.com>
2026-08-13 21:02:52 +0900

    설정 팝업 풀필요 요소 제거

--- Assets/Scripts/MilliRhythm/UI/ConfigUI/ConfigPopup.cs
+++ Assets/Scripts/MilliRhythm/UI/ConfigUI/ConfigPopup.cs
@@ -25,12 +25,8 @@ namespace MilliRhythm.UI.ConfigUI
 		[SerializeField] private Toggle koreanToggle;
 		[SerializeField] private Toggle englishToggle;
 
-		[SerializeField] private QuitGamePopup quitGamePopup;
 		[SerializeField] private CreditPopup creditPopup;
-		[SerializeField] private NavigatableButton quitGameButton;
 		[SerializeField] private NavigatableButton creditButton;
-
-		[SerializeField] private NavigatableIntField judgeOffset;
 
 		[SerializeField] private Toggle wasdToggle;
 		[SerializeField] private Toggle sdklToggle;
@@ -50,13 +46,10 @@ namespace MilliRhythm.UI.ConfigUI
 			koreanToggle.onValueChanged.AddListener(OnKoSelected);
 			englishToggle.onValueChanged.AddListener(OnEnSelected);
 
-			quitGameButton.onClick.AddListener(DisplayQuitGamePopup);
 			creditButton.onClick.AddListener(DisplayConfigGamePopup);
 
 			wasdToggle.onValueChanged.AddListener(OnWasdSelected);
 			sdklToggle.onValueChanged.AddListener(OnSdklSelected);
-
-			judgeOffset.OnValueChanged += OnChangeJudgeOffset;
 
 			Refresh();
 		}
@@ -71,13 +64,10 @@ namespace MilliRhythm.UI.ConfigUI
 			koreanToggle.onValueChanged.RemoveListener(OnKoSelected);
 			englishToggle.onValueChanged.RemoveListener(OnEnSelected);
 
-			quitGameButton.onClick.RemoveListener(DisplayQuitGamePopup);
 			creditButton.onClick.RemoveListener(DisplayConfigGamePopup);
 
 			wasdToggle.onValueChanged.RemoveListener(OnWasdSelected);
 			sdklToggle.onValueChanged.RemoveListener(OnSdklSelected);
-
-			judgeOffset.OnValueChanged -= OnChangeJudgeOffset;
 		}
 
 		protected override void Set()
@@ -98,8 +88,6 @@ namespace MilliRhythm.UI.ConfigUI
 			koreanToggle.SetIsOnWithoutNotify(language == LanguageType.Korean);
 			japaneseToggle.SetIsOnWithoutNotify(language == LanguageType.Japanese);
 			englishToggle.SetIsOnWithoutNotify(language == LanguageType.English);
-
-			judgeOffset.SetInitialValue(ConfigManager.Instance.Config.JudgeOffset);
 
 			wasdToggle.SetIsOnWithoutNotify(ConfigManager.Instance.Config.KeyLayout == KeyLayout.WASD);
 			sdklToggle.SetIsOnWithoutNotify(ConfigManager.Instance.Config.KeyLayout == KeyLayout.SDKL);
@@ -137,12 +125,6 @@ namespace MilliRhythm.UI.ConfigUI
 		{
 			if (!selected) return;
 			ConfigManager.Instance.ChangeKeyLayout(layout);
-		}
-
-
-		private void OnChangeJudgeOffset(int offset)
-		{
-			ConfigManager.Instance.ChangeJudgeOffset(offset);
 		}
 
 		public override void OnNavigate(Vector2 value)
@@ -196,22 +178,6 @@ namespace MilliRhythm.UI.ConfigUI
 			{
 				isPopupOpened = true;
 				await creditPopup.Display(null);
-				isPopupOpened = false;
-			})();
-		}
-
-		public void DisplayQuitGamePopup()
-		{
-			if (isPopupOpened) return;
-			UniTask.Action(async () =>
-			{
-				isPopupOpened = true;
-				var result = await quitGamePopup.Display(null);
-				if (result.Result == PopupResult.Confirm)
-				{
-					Application.Quit();
-				}
-
 				isPopupOpened = false;
 			})();
 		}
